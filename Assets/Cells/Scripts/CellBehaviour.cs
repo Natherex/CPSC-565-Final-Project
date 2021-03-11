f1b3429f18d617e67477eda02406074c1e36db78
Isha <isha_afzaal8@outlook.com>
2021-03-11 09:39:57 +0000

    Everything working well; have refactored CellBehavior.cs and included references in the comments. Am thinking of extracting the quorum-sensing behavior methods into another class but am on the fence aboout that

--- Assets/Cells/Scripts/CellBehaviour.cs
+++ Assets/Cells/Scripts/CellBehaviour.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections;
+﻿/*
+ * Authors: Isha Afzaal, Sammy Elrafih, Ainslie Veltheon
+ * Info: CellBehavior.cs specifies agent behaviour in the quorum-sensing system of l. pneumophila.
+ *       Cells use quorum-sensing to understand their population's cell density. Once the cell density passes a
+ *       certain threshold, agents start exhibiting emergent behaviour.
+ * References:
+ *  Using timers in Unity: https://answers.unity.com/questions/1453479/how-to-slow-down-random-enemy-spawn.html
+ *  Making objects spawn other objects in Unity: https://answers.unity.com/questions/420177/how-do-i-make-an-object-spawn-another-one.html
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CellBehaviour : MonoBehaviour
 {
-    // Reference to cell prefabs used for replication
-  //  public GameObject cell;
- //   public GameObject cell_prefab;
-    private Rigidbody physicsBody;
+
+    // Cell Reproduction Configuration
     private int reproduction_limit = 5;
     private int cells_reproduced = 0;
-    private bool quorum_sensing_switch = false;
-    private float targetTime = 5.0f;   //how long it will count down
-    Vector3 force;
 
-    // Cell individual energy level initially starts at 100
+    // Quorum Sensing (QS) Configuration
+    private int radius = 1;
+    private int threshold_value = 5;
+
+    // Cell Movement/Life and Death Control
+    private Rigidbody physicsBody;
+    Vector3 force;
+    private bool quorum_sensing_switch = false;
+    private float target_time = 5.0f;
     private int energy = 4000;
    
-
+    // Can this constructor be removed?
     public void constructor()
     {
 
@@ -32,71 +45,17 @@ public class CellBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Make the cell move
         force = new Vector3 (Random.Range(-10,10),-0.1f,Random.Range(-10,10));
         physicsBody.AddForce(force/500);
-        
-        // Sensing Signalling Molecules: Check the total number of bacteria around me. Multiply and increment the total signalling molecule value
-        int radius = 1;
-        int threshold_value = 5;
-        Collider[] nearby_objects = Physics.OverlapSphere(transform.position, radius);
 
-        // Threshold value check
-        if (nearby_objects.Length >= threshold_value)
-        { 
-            Debug.Log("Activating emergent behavior.");
-            emergent_behavior();
-            quorum_sensing_switch = true;
-        }
-        // Theshold value not reached: Divide and create new bacteria
-        else if (!quorum_sensing_switch)
-        {
-            // Wait a bit before spawning
-            targetTime -= Time.deltaTime; //reduce target time by 1 every second
-
-            if (targetTime <= 0.0f)       //timer ended
-            {
-                targetTime = 5.0f;
-              //  timerEnded();
-                Debug.Log("Creating new bacteria.");
-
-                if (cells_reproduced < reproduction_limit)
-                {
-                    // Create new game object
-                    createCell(transform.position);
-                    cells_reproduced++;
-
-                    // Reduce energy
-                    energy = energy - 10;
-                }
-                //  Instantiate(cell, transform.position, Quaternion.identity);
-                //  Instantiate(bacteria_prefab);
-                // Update global signalling molecule value
-                Debug.Log("Update signalling molecule +1");
-            
-            }
-
-        }
-
-        // Consume energy
-        energy = energy - 1;
-     //   Debug.Log("1");
-        // Take energy from agar
-        if (globals.total_agar > 2)
-        {
-            energy = energy + 2;Debug.Log("2");
-            globals.total_agar = globals.total_agar - 2;
-        }
-
-        // Check if this cell has died
-        if (energy <= 0)
-        {
-            Destroy(gameObject);
-        }
-
-
-
+        quorum_sensing();
+        consume_energy();
     }
 
+    /*
+     * Specify the emergent behavior the cells should have here. For now, will simply change cell colours
+     */
     private void emergent_behavior ()
     {
         // Change the current agent cell's colour
@@ -104,16 +63,77 @@ public class CellBehaviour : MonoBehaviour
         quorum_sensing_switch = true;
     }
 
-    // Adapted from Sammy's cellSpawner.cs
+    /*
+     * Adapted from Sammy's cellSpawner.cs
+     */
     private void createCell(Vector3 spawn_location)
     {
         var go = GameObject.Find("Cell(Clone)");
         if (go != null)
         {
-            // GameObject new_cell = Instantiate(go, transform.position, Quaternion.identity) as GameObject;
             Instantiate(go, spawn_location, Quaternion.identity);
-           // new_cell.transform.position = spawn_location;
         }
-        else { Debug.Log("Failed to find Cell."); }
+    }
+
+    /*
+     * Quorum-sensing: Agents detect cell density and exhibit emergent behavior if threshold reached or
+     * reproduce if threshold not reached
+     */
+    private void quorum_sensing ()
+    {
+        // QS Step 1: Sense the number of surrounding bacteria
+        Collider[] nearby_objects = Physics.OverlapSphere(transform.position, radius);
+
+        // QS Step 2: Check surrounding population cell density
+        if (!quorum_sensing_switch)
+        {
+            // Case 1: Cell density has reached the threshold value
+            if (nearby_objects.Length >= threshold_value)
+            {
+                Debug.Log("Activating emergent behavior");
+                emergent_behavior();
+            }
+
+            // Case 2: Cell density lower than threshold value - reproduce a new cell
+            else
+            {
+                // Wait for some time before spawning
+                target_time -= Time.deltaTime;
+                if (target_time <= 0.0f)
+                {
+                    target_time = 5.0f;
+                    Debug.Log("Creating New Bacteria.");
+
+                    if (cells_reproduced < reproduction_limit)
+                    {
+                        // Create new game object
+                        createCell(transform.position);
+                        cells_reproduced++;
+                        energy = energy - 10;
+                    }
+                }
+            }
+        }
+    }
+
+    /*
+     * As the cells move, they consume energy and will die upon its depletion
+     */
+    private void consume_energy ()
+    {
+        Debug.Log("Consuming Energy.");
+        energy = energy - 1;
+
+        if (globals.total_agar > 2)
+        {
+            energy = energy + 2; Debug.Log("2");
+            globals.total_agar = globals.total_agar - 2;
+        }
+
+        // QS Step 3: Cells die upon having no energy
+        if (energy <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
