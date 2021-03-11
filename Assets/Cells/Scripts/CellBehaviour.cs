77c3a47e4f79e3c3d8ca15f182ec9fa827b7f84c
Isha <isha_afzaal8@outlook.com>
2021-03-11 03:40:51 +0000

    Added cell behaviour; cells sense surrounding population density by counting the number of cells in radius and turn red when emergent behavior occurs

--- Assets/Cells/Scripts/CellBehaviour.cs
+++ Assets/Cells/Scripts/CellBehaviour.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CellBehaviour : MonoBehaviour
 {
+    // Reference to cell prefabs used for replication
+    public GameObject bacteria_prefab;
+    
     private Rigidbody physicsBody;
     Vector3 force;
-    // Start is called before the first frame update
+   
 
     public void constructor()
     {
 
     }
 
+    // Start is called before the first frame update
     void Start()
     {
         physicsBody = GetComponent<Rigidbody>();
@@ -23,5 +27,36 @@ public class CellBehaviour : MonoBehaviour
     {
         force = new Vector3 (Random.Range(-10,10),-0.1f,Random.Range(-10,10));
         physicsBody.AddForce(force/500);
+        
+        // Sensing Signalling Molecules: Check the total number of bacteria around me. Multiply and increment the total signalling molecule value
+        int radius = 1;
+        int threshold_value = 5;
+        Collider[] nearby_objects = Physics.OverlapSphere(transform.position, radius);
+
+        // Threshold value check
+        if (nearby_objects.Length >= threshold_value) 
+        { 
+            Debug.Log("Activating emergent behavior.");
+            emergent_behavior();
+        }
+        // Theshold value not reached: Divide and create new bacteria
+        else
+        {
+            Debug.Log("Creating new bacteria.");
+
+            // Create new game object
+            //  Instantiate(bacteria_prefab, transform.position, Quaternion.identity);
+            Instantiate(bacteria_prefab);
+            // Update global signalling molecule value
+            Debug.Log("Update signalling molecule +1");
+
+        }
+
+    }
+
+    private void emergent_behavior ()
+    {
+        // Change the current agent cell's colour
+        GetComponent<Renderer>().material.color = Color.red;
     }
 }
