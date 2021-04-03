5aec061458fdf8d668d84e578474c8187b29385b
Ainslie Velthoen <ainslie.velthoen@ucalgary.ca>
2021-04-03 04:33:00 +0000

    Very basic UI updated

---
+++ Assets/Environment/Scripts/InputUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputUI : MonoBehaviour
+{
+    public string value;
+    public GameObject inputField;
+
+    public void storeValue()
+    {
+        value = inputField.GetComponent<Text>().text;
+        Debug.Log(value);
+    }
+    
+}
